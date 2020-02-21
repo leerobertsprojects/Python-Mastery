6c5f36e2d47bc316a3eb2952aad334dc5be1e50f
leer <leesroberts@virginmedia.com>
2020-02-21 09:37:10 +0000

    first commit

---
+++ Basics 2/conditional logic- if statements.py
@@ -0,0 +1,18 @@
+is_old = False
+is_licenced = False
+
+# if is_old and is_licenced:
+#     print('You are old enough & licenced to drive')
+# # elif is_licenced:
+# #     print('You are licenced to drive')
+# else:
+#     print('You are not old enough to drive or do not have licence to drive')
+
+# Ternary Operator
+
+# condition_if_true if condition else condition_if_else
+
+print('You are old enough to drive') if is_old else print('You are not old enough to drive')
+
+
+
---
+++ Excercises/Logical operators.py
@@ -0,0 +1,10 @@
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+    print('You are a master Magician')
+elif is_magician and not is_expert:
+    print('at least you\'re getting there')
+elif not is_magician:
+    print('you need magic powers')
+
