0ceed8f300baef9d0a86babc1a1527673984ef8b
leeroberts <leesroberts@virginmedia.com>
2020-02-24 21:19:28 +0000

    Mac commit

---
+++ Advanced Python Concepts/Object Orientated Programing/OOP.py
@@ -0,0 +1,18 @@
+# Everything in python is an object using classes
+
+class PlayerCharacter:
+
+    # Class Object attribute
+    membership = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def Run(self):
+        print(f'Run {self.name}')
+
+
+player1 = PlayerCharacter('Lee', 38)
+print(player1.membership)
+player1.Run()
