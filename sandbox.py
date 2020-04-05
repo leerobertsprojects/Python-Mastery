b73268e865abeb0d4151c093fffba3f8ef358ed4
leeroberts <leesroberts@virginmedia.com>
2020-04-05 16:07:33 +0100

    Mac commit

---
+++ sandbox.py
@@ -0,0 +1,14 @@
+class Practice:
+    def __init__(self, name, age, hp):
+        self.name = name
+        self.age = age
+        self.hp = hp
+
+    def EnemyAttack(self):
+        
+
+
+
+Player1 = Practice('lee', 20, 200)
+
+print(f'Hello {Player1.name} Your age is {Player1.age}, You have {Player1.hp} Hit Points available')
