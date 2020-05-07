7a3e7f5a8dc47b836bd2376162432655999b92c7
leeroberts <leesroberts@virginmedia.com>
2020-05-07 14:57:04 +0100

    Mac commit

--- Scripting With Python/Images/images.py
+++ Scripting With Python/Images/images.py
@@ -1,12 +1,17 @@
 import os
 from PIL import Image, ImageFilter
-basedir = os.path.dirname(__file__)
-img = Image.open(f'{basedir}/pokedex/bulbasaur.jpg')
-filtered_image = img.convert('L')
-filtered_image.save(f"{basedir}/grey.png", "png")
-print(basedir)
-# print(img.format)
-# print(img.size)
-# print(img.mode)
+# basedir = os.path.dirname(__file__)
+# img = Image.open(f'{basedir}/pokedex/bulbasaur.jpg')
+# filtered_image = img.convert('L')
+# new_img = filtered_image.resize((400, 400))
+# new_img.save(f"{basedir}/resize.png", "png")
+# box = (100, 100, 400, 400)
+# region = filtered_image.crop(box)
+# region.save(f"{basedir}/region.png", "png")
 
+basedir = os.path.dirname(__file__)
+img = Image.open(f'{basedir}/tech.jpg')
+img.thumbnail((400, 400))
+img.save(f"{basedir}/tech.png", "png")
+print(img.size)
 
